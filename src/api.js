5191eaceb677de7bae8dba2bb83a6549ce39cafb
Errol Schwartz <errol.schwartz@frogdesign.com>
2015-08-07 14:07:23 -0400

    started static api

---
+++ src/api.js
@@ -0,0 +1,14 @@
+var api = {
+    rooms: [
+        {
+            id: 0,
+            name: 'Best Room One',
+            audience: [
+                '123312323'
+            ],
+            djs: [
+                '927434983'
+            ]
+        }
+    ]  
+};
